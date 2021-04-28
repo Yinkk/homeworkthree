5e049888fee18f0fba5f86ea21f04c8f24f08a1e
yeunyong <yeunyong.ka@gmail.com>
2021-04-28 18:01:43 +0700

    .gitignore fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopBannerComponent } from './top-banner/top-banner.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBannerComponent,
+    NavBarComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
